53e8593a73183057a62e8f368ab999674d818763
Kristen Moore <kristen@codefu.org>
2017-05-29 16:15:18 -0500

    Draw image onto canvas

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+function main() {
+  var canvas = document.getElementById("canvas");
+  var context = canvas.getContext('2d');
+  var image = new Image();
+  image.addEventListener("load", function() {
+    context.drawImage(image, 50, 50, 39, 34);
+  });
+  image.src = "ship.png";
+}
+
 if (document.readyState === "interactive" || document.readyState === "complete") {
   main();
 } else {
