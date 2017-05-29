83076a1d689f6e6b942b891ad4aabf26fedd2c8a
Kristen Moore <kristen@codefu.org>
2017-05-29 17:23:41 -0500

    Write ship class and draw function; move drawing responsibility to draw function

--- main.js
+++ main.js
@@ -1,9 +1,31 @@
+"use strict";
+
+const ROTATION_SPEED = (2 * Math.PI) / 2000;
+
+class Ship {
+  constructor(image, x, y) {
+    this.image = image;
+    this.x = x;
+    this.y = y;
+    this.rotation = 0.0;
+  }
+
+  draw(context, msElapsed) {
+    context.setTransform(1, 0, 0, 1, 0, 0);
+    context.translate(this.x, this.y);
+    this.rotation = (this.rotation + msElapsed * ROTATION_SPEED) % (2 * Math.PI);
+    context.rotate(this.rotation);
+    context.drawImage(this.image, -this.image.width / 2, -this.image.height / 2, this.image.width, this.image.height);
+  }
+}
+
 function main() {
   var canvas = document.getElementById("canvas");
   var context = canvas.getContext('2d');
-  var image = new Image();
+  var image = new Image(40, 34);
+  var ship = new Ship(image, 50, 50);
   image.addEventListener("load", function() {
-    context.drawImage(image, 50, 50, 39, 34);
+    ship.draw(context, 0);
   });
   image.src = "ship.png";
 }
